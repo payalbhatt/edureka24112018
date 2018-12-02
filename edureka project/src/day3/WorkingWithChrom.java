8d25a8fedb25582b7dddd83353618e06978125e1
payal <payal@payalbhatt-PC.fios-router.home>
2018-12-02 13:10:04 -0500

    day3

--- edureka project/src/day3/WorkingWithChrom.java
+++ edureka project/src/day3/WorkingWithChrom.java
@@ -8,13 +8,13 @@ public class WorkingWithChrom {
 	public void invokeBrowser() {
 		
 		System.setProperty("webdriver.chrome.driver",
-				"C:/Users/payal/Desktop/PAYALQA/chromedriver");
+				"C:\\Users\\payal\\Desktop\\PAYALQA\\QA\\chromedriver.exe");
 		
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		
-		driver.get("https://www.edureka.co");
+		driver.get("https://www.edureka.co/");
 		
 	
 	}
